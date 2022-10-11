4a191eabce23f37fb39dcc26f9087591a62bd0a3
Manuel <manuhg13@gmail.com>
2022-10-11 10:14:11 +0000

    h

---
+++ Tema 1/Calculadora/JS/funciones.js
@@ -0,0 +1,20 @@
+function calculadora(operador,n1,n2) {
+    switch (operador) {
+        case '*':
+            
+            break;
+        case '/':
+            
+            break;
+        case '-':
+            
+            break;
+        case '+':
+            
+            break;
+    
+        default:
+            break;
+    }
+    
+}
