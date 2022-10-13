04ec3fc4597c5ddb1a5b48be21485b8e6ba878bf
Manuel <manuhg13@gmail.com>
2022-10-13 07:38:59 +0000

    f

--- Tema 1/Calculadora/JS/funciones.js
+++ Tema 1/Calculadora/JS/funciones.js
@@ -1,6 +1,6 @@
-function suma() {
-    if (condition) {
-        
+function suma(n1,n2) {
+    if ((isNaN(n1)) || (isNaN(n2))) {
+        throw new Error("No son números")
     }
     return n1+n2;
 }
@@ -12,4 +12,20 @@ function division(n1,n2) {
 }
 function multiplicacion(n1,n2) {
     return n1*n2;
+}
+
+function calculadora() {
+    const forma=document.getElementById('formulario');
+    const n1 = formulario['primerDigito'].value;
+    const n2 = formulario['segundoDigito'].value;
+
+    try {
+        document.getElementById('resultado').value=`${suma(n1,n2)}`
+        document.getElementById('resultado').value=`${resta(n1,n2)}`
+        document.getElementById('resultado').value=`${division(n1,n2)}`
+        document.getElementById('resultado').value=`${multiplicacion(n1,n2)}`
+    } catch (error) {
+        document.getElementById('resultado').style.color='color';
+        document.getElementById('')
+    }
 }
