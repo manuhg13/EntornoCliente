d87fd287e98ddcc069deb7d299e8a32dae0d49e5
Manuel <manuhg13@gmail.com>
2022-10-13 08:00:46 +0000

    g

--- Tema 1/Calculadora/JS/funciones.js
+++ Tema 1/Calculadora/JS/funciones.js
@@ -5,12 +5,25 @@ function suma(n1,n2) {
     return n1+n2;
 }
 function resta(n1,n2) {
+    if ((isNaN(n1)) || (isNaN(n2))) {
+        throw new Error("No son números")
+    }
     return n1-n2;
 }
 function division(n1,n2) {
+    if ((isNaN(n1)) || (isNaN(n2))) {
+        throw new Error("No son números");
+    } else if(n2>n1){
+        throw new Error("Divisor mayor que el dividendo");
+    } else if(n2==0){
+        throw new Error("No se puede dividir entre 0");
+    }
     return n1/n2;
 }
 function multiplicacion(n1,n2) {
+    if ((isNaN(n1)) || (isNaN(n2))) {
+        throw new Error("No son números");
+    }
     return n1*n2;
 }
 
@@ -20,12 +33,26 @@ function calculadora() {
     const n2 = formulario['segundoDigito'].value;
 
     try {
-        document.getElementById('resultado').value=`${suma(n1,n2)}`
-        document.getElementById('resultado').value=`${resta(n1,n2)}`
-        document.getElementById('resultado').value=`${division(n1,n2)}`
-        document.getElementById('resultado').value=`${multiplicacion(n1,n2)}`
+        switch (document.getElementById()) {
+            case document.getElementById('suma'):
+                document.getElementById('resultado').value=`${suma(n1,n2)}`
+                break;
+            case document.getElementById('resta'):
+                document.getElementById('resultado').value=`${resta(n1,n2)}`
+                break;
+            case document.getElementById('division'):
+                document.getElementById('resultado').value=`${division(n1,n2)}`
+                break;
+            case document.getElementById('multiplicacion'):
+                document.getElementById('resultado').value=`${multiplicacion(n1,n2)}`
+                break;
+        
+            default:
+
+                break;
+        }     
     } catch (error) {
         document.getElementById('resultado').style.color='color';
-        document.getElementById('')
+        document.getElementById('resultado').value=error;
     }
 }
