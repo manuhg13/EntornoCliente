25807497a4fe6763421e85005fb0cad35dba6709
manuhg13 <manuhg13@gmail.com>
2022-10-15 09:36:09 +0000

    a

--- Tema 1/ArraysMeses/JS/funciones.js
+++ Tema 1/ArraysMeses/JS/funciones.js
@@ -1,7 +1,10 @@
+const meses=["Enero","Febrero","Marzo","Abril","Mayo","Junio","Julio","Agosto","Septiembre","Octubre","Noviembre","Diciembre"];
 let i=0;
 
-function escribirMeses(meses,i) {
-    document.write(`<ul><li>${meses[i]}</li></ul>`);
+function escribirMeses() {
+    const ul = document.getElementById("lista");
+    const li= document.createElement("li");
+    li.appendChild(document.createTextNode(meses[i]));
+    ul.appendChild(li);
+    i++;
 }
-
-const meses=["Enero","Febrero","Marzo","Abril","Mayo","Junio","Julio","Agosto","Septiembre","Octubre","Noviembre","Diciembre"];
