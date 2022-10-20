bef0b03085da00ca1ca7207594455d6be526f5c2
manuhg13 <manuhg13@gmail.com>
2022-10-20 18:08:15 +0000

    d

--- Tema 1/RojoVerde/JS/pulsacion.js
+++ Tema 1/RojoVerde/JS/pulsacion.js
@@ -1,7 +1,9 @@
 let verde= document.getElementById('verde');
 let rojo= document.getElementById('rojo');
+
 let pulVerde=0;
 let pulRojo=0;
+
 rojo.appendChild(document.createTextNode(pulRojo));
 verde.appendChild(document.createTextNode(pulVerde));
 
