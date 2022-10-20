08864acac37a5336a563ba7ad6090a44e2003278
manuhg13 <manuhg13@gmail.com>
2022-10-20 17:35:36 +0000

    d

---
+++ Tema 1/RojoVerde/JS/pulsacion.js
@@ -0,0 +1,25 @@
+const verde=document.createElement('button');
+verde.setAttribute('id','verde');
+verde.style.backgroundColor="green";
+verde.style.display="block";
+verde.style.color="white";
+let pulVerde=0;
+
+//----------------------------------
+
+const rojo=document.createElement('button');
+rojo.setAttribute('id','rojo');
+rojo.style.display="block";
+rojo.style.backgroundColor="red";
+rojo.style.color="white";
+let pulRojo=0;
+
+function masVerde() {
+    pulVerde++;
+    verde.appendChild(document.createTextNode(pulVerde));
+}
+
+function masRojo() {
+    pulRojo++;
+    rojo.appendChild(document.createTextNode(pulRojo));
+}
