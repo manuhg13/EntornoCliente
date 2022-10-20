c0883409261f0b2fdb7b8c1d839bee84acf709c2
Manuel <manuhg13@gmail.com>
2022-10-20 07:34:08 +0000

    6

--- Tema 1/DOM/JS/dom.js
+++ Tema 1/DOM/JS/dom.js
@@ -19,22 +19,47 @@ console.log(document.forms);
 console.log(document.querySelectorAll('input'));
 
 //6. Sólo los inputs con nombre sexo
-let inputs= document.getElementsByTagName('INPUTS');
-console.log(inputs.getElementByName("sexo"));
+console.log(document.getElementsByName("sexo"));
+
+//7. Los itens de lista de la calse 'important' (sólo los <li>)
+console.log(document.querySelectorAll('li.important'));
+
 
 //----------Ejercicio 2-------------------------//
 
 //1. El primer párrafo que hay dentro del <div> 'lipsum'
-let primeroP= divs.firstChild;
+let primeroP= divs.firstElementChild;
 console.log(primeroP);
 
 //2. El segundo párrafo de 'lipsum'
-let segundoP=divs.nextElementSibling;
+let segundoP=primeroP.nextElementSibling;
 console.log(segundoP);
 
 //3. El último item de la lista 
 let lista = document.querySelector('ul');
-console.log(lista.lastChild);
+console.log(lista.lastElementChild);
+
+//4. El elemento label de "Escoge sexo"
+let radios=document.getElementsByName("sexo");
+console.log(radios[0].parentElement); 
+
+//------------Ejercicio 3---------------//
+
+//1. El innerHTML de la etiqueta "Escoge sexo"
+console.log(radios[0].parentElement.innerText); 
+
+//2. El textContent de esa etiqueta
+console.log(radios[0].parentElement.textContent); 
+
+//3. El valor del primer input de sexo
+console.log(radios[0].value); 
+
+//4. El valor del sexo que este seleccionado 
+console.log(document.querySelector('input[name="sexo"]:checked').value); 
+
+
+
+
 
 
 
