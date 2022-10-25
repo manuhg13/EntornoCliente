f7281a769d26b9ba08d4a6324b8e411729d93d7a
manuhg13 <manuhg13@gmail.com>
2022-10-25 19:52:01 +0000

    d

--- Tema 1/Ejercicio6/JS/dibujar.js
+++ Tema 1/Ejercicio6/JS/dibujar.js
@@ -2,32 +2,37 @@
 function pintarRombo() {
 
     const filas=parseInt(document.getElementById('filas').value);
+    const div= document.createElement('div');
+
+    if(div.innerHTML != "" ){
+        div.innerHTML="";
+    }
     
 
     for (i=1; i <= filas ; i++) { 
         for (let espacios=1; espacios<=filas-i; espacios++) { 
-            document.body.innerHTML+="&nbsp;&nbsp";
+            div.innerHTML+="&nbsp;&nbsp";
         }
         for (let asteriscos=1; asteriscos <=(i*2)-1; asteriscos++) {    
             if (asteriscos==1 || asteriscos==(i*2)-1) {
-                document.body.innerHTML+="*";
+                div.innerHTML+="*";
             }else {
-                document.body.innerHTML+="&nbsp;&nbsp;";
+                div.innerHTML+="&nbsp;&nbsp;";
             }        
         } 
         document.body.innerHTML+="<br>";
     }
     for (let i=filas; i >= 1 ; i--) { 
         for (let espacios=1; espacios<=filas-i; espacios++) { 
-            document.body.innerHTML+="&nbsp;&nbsp";
+            div.innerHTML+="&nbsp;&nbsp";
         }
         for (let asteriscos=1; asteriscos <=(i*2)-1; asteriscos++) {    
             if (asteriscos==1 || asteriscos==(i*2)-1) {
-                document.body.innerHTML+="*";
+                div.innerHTML+="*";
             }else {
                 document.body.innerHTML+="&nbsp;&nbsp;";
             }
         } 
-        document.body.innerHTML+="<br>";
+        div.innerHTML+="<br>";
     }
 }
