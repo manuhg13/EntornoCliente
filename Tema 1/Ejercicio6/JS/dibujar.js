ea84d41e8976a347ae37318e3f5878459a9fabf3
manuhg13 <manuhg13@gmail.com>
2022-10-25 18:28:27 +0000

    s

--- Tema 1/Ejercicio6/JS/dibujar.js
+++ Tema 1/Ejercicio6/JS/dibujar.js
@@ -4,23 +4,58 @@ function pintarRombo() {
     let arriba=(filas/2)+1;
     let abajo=filas-arriba;
 
-    for (let i=1; i <= arriba ; $i++) {
+    let parrafo=document.createElement('pre');
+    let salto=document.createElement('br');
+    
 
+
+    /*for (let i=1; i <= arriba ; i++) {
+        let linea='';
         for (let espacios=1; espacios<=arriba-i; espacios++) { 
-           document.write("&nbsp;&nbsp");
+            linea+=" ";
         }
         for (let asteriscos=1; asteriscos <=(i*2)-1; asteriscos++) {    
-            document.write("*");
+            linea+="*";
         } 
-        document.writeln;
+        document.body.appendChild(salto);
+        document.body.appendChild(parrafo.appendChild(document.createTextNode(linea)));
     }
     for (let i=abajo; i >= 1 ; i--) { 
-        for (let espacios=1; espacios<=3-$i; espacios++) { 
-            document.write("&nbsp;&nbsp");
+        let linea='';
+        for (let espacios=1; espacios<=3-i; espacios++) { 
+            linea+=" ";
         }
         for (let asteriscos=1; asteriscos <=(i*2)-1; asteriscos++) {    
-            document.write("*");
+            linea+="*";
         } 
-        document.writeln;
+        document.body.appendChild(salto);
+        document.body.appendChild(parrafo.appendChild(document.createTextNode(linea)));
+    }*/
+
+    for (i=1; i <= arriba ; i++) { 
+        for (let espacios=1; espacios<=arriba-i; espacios++) { 
+            document.innerHTML+="&nbsp;&nbsp";
+        }
+        for (let asteriscos=1; asteriscos <=(i*2)-1; asteriscos++) {    
+            if (asteriscos==1 || asteriscos==($i*2-1)) {
+                document.innerHTML+="*";
+            }else {
+                document.innerHTML+="&nbsp;&nbsp;";
+            }        
+        } 
+        document.innerHTML+="<br>";
+    }
+    for (let i=abajo; i >= 1 ; i--) { 
+        for (let espacios=1; espacios<=3-i; espacios++) { 
+            document.innerHTML+="&nbsp;&nbsp";
+        }
+        for (let asteriscos=1; asteriscos <=(i*2)-1; asteriscos++) {    
+            if (asteriscos==1 || asteriscos==(i*2-1)) {
+                document.innerHTML+="*";
+            }else {
+                document.innerHTML+="&nbsp;&nbsp;";
+            }
+        } 
+        document.innerHTML+="<br>";
     }
 }
