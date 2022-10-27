565d7271525509ba728a9b3ca5b01362b90f678f
manuhg13 <manuhg13@gmail.com>
2022-10-27 10:09:46 +0000

    d

---
+++ Tema 1/Ejercicio7/JS/dibujar.js
@@ -0,0 +1,47 @@
+
+function pintarRombo() {
+
+    const filas=parseInt(document.getElementById('filas').value);
+    let arriba;
+    let abajo;
+    if (filas%2==0){
+        arriba=filas/2;
+        abajo=filas/2;
+    }else {
+        arriba=parseInt(filas/2)+1;
+        abajo=arriba-1;
+    }
+    const div= document.getElementById('aqui');
+   
+    if(div.innerHTML != "" ){
+        div.innerHTML="";
+    }
+    
+
+    for (let i=1; i <= arriba ; i++) { 
+        for (let espacios=1; espacios<=arriba-i; espacios++) { 
+            div.innerHTML+="&nbsp;&nbsp";
+        }
+        for (let asteriscos=1; asteriscos <=(i*2)-1; asteriscos++) {    
+            if (asteriscos==1 || asteriscos==(i*2)-1) {
+                div.innerHTML+="*";
+            }else {
+                div.innerHTML+="&nbsp;&nbsp;";
+            }        
+        } 
+        div.innerHTML+="<br>";
+    }
+    for (let i=abajo; i >= 1 ; i--) { 
+        for (let espacios=1; espacios<=abajo-i; espacios++) { 
+            div.innerHTML+="&nbsp;&nbsp";
+        }
+        for (let asteriscos=1; asteriscos <=(i*2)-1; asteriscos++) {    
+            if (asteriscos==1 || asteriscos==(i*2)-1) {
+                div.innerHTML+="*";
+            }else {
+                div.innerHTML+="&nbsp;&nbsp;";
+            }
+        } 
+        div.innerHTML+="<br>";
+    }
+}
