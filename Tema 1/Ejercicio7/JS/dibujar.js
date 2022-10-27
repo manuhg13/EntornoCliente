36cb22e571f2c288ecd8731142f7d2aa51f13061
manuhg13 <manuhg13@gmail.com>
2022-10-27 10:42:01 +0000

    s

--- Tema 1/Ejercicio7/JS/dibujar.js
+++ Tema 1/Ejercicio7/JS/dibujar.js
@@ -9,7 +9,7 @@ function pintarRombo() {
         abajo=filas/2;
     }else {
         arriba=parseInt(filas/2)+1;
-        abajo=arriba-1;
+        abajo=parseInt(filas/2);;
     }
     const div= document.getElementById('aqui');
    
