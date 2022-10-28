a9ce7ec2905ea954f2a0df682003f1d86cf48687
Manuel <manuhg13@gmail.com>
2022-10-28 09:09:32 +0000

    4

--- Tema 1/Ejercicio7/JS/dibujar.js
+++ Tema 1/Ejercicio7/JS/dibujar.js
@@ -1,9 +1,15 @@
 
 function pintarRombo() {
-
+    //Cogemos los elementos del HTML 
     const filas=parseInt(document.getElementById('filas').value);
+    const div= document.getElementById('aqui');
+
+    //Declaramos la division del rombo en dos partes  
     let arriba;
     let abajo;
+
+    /*Comprobamos si el número de filas es par o impar,
+    para la asignacion de más filas arriba si fuera impar*/
     if (filas%2==0){
         arriba=filas/2;
         abajo=filas/2;
@@ -11,27 +17,40 @@ function pintarRombo() {
         arriba=parseInt(filas/2)+1;
         abajo=parseInt(filas/2);;
     }
-    const div= document.getElementById('aqui');
-   
+    
+    //Limpiamos el div si NO estuviera vacío
     if(div.innerHTML != "" ){
         div.innerHTML="";
     }
     
-
+    //Comenzamos escribiendo la líneas de arriba 
     for (let i=1; i <= arriba ; i++) { 
+
+        //En primer lugar, escribiremos los espacios que le separarán del margen 
+
         for (let espacios=1; espacios<=arriba-i; espacios++) { 
+            /*La fuente predeterminada de HTML NO es monoespaciada, 
+            por lo que se debe poner dos espacios en lugar de uno*/
             div.innerHTML+="&nbsp;&nbsp";
         }
-        for (let asteriscos=1; asteriscos <=(i*2)-1; asteriscos++) {    
+        //En segundo lugar, se colocan los asteriscos
+        for (let asteriscos=1; asteriscos <=(i*2)-1; asteriscos++) {  
+            /*Sin esta condición, rellenaría toda la fila de asteriscos. 
+            Gracias a ella podemos escribir solo un asterisco al principio del bucle y al final*/   
             if (asteriscos==1 || asteriscos==(i*2)-1) {
                 div.innerHTML+="*";
             }else {
                 div.innerHTML+="&nbsp;&nbsp;";
             }        
         } 
+        /*Esto genera un salto de linea al acabar de 
+        escribir los espacios y asteriscos que corresponden*/
         div.innerHTML+="<br>";
     }
+    //Esta ver haremos la parte de abajo del rombo
     for (let i=abajo; i >= 1 ; i--) { 
+        /*Si es impar tiene que añadir un espacio más en la primera fila que escribe,
+        en cambio si es par, debe estar alineado con la ultima fila escrita de la parte de arriba */ 
         if (filas%2==0) {
             for (let espacios=1; espacios<=abajo-i; espacios++) { 
                 div.innerHTML+="&nbsp;&nbsp";
