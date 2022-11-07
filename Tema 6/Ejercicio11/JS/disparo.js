8cb899e1d0922d167bb3ebe81df0d097ef74ce56
manuhg13 <manuhg13@gmail.com>
2022-11-07 15:35:28 +0000

    c

--- Tema 6/Ejercicio11/JS/disparo.js
+++ Tema 6/Ejercicio11/JS/disparo.js
@@ -2,8 +2,12 @@ let xDiana=50;
 let velocidad=10;
 
 function moverDiana() {
-    xDiana+=velocidad,
+    xDiana+=velocidad;
     document.getElementById('idDiana').style.left=`${xDiana}px`
+    if (document.getElementById('idDiana').style.left==document.getElementById('carrusel').style.width){
+        document.getElementById('idDiana').style.left=`-${xDiana}px`
+
+    }
 }
 
 let intervalDiana=setInterval(moverDiana,50);
