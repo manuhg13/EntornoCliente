bcb514f0a705d3fb6ee71f929d30ea9e52ca2963
Manuel <manuhg13@gmail.com>
2022-11-07 12:09:44 +0000

    d

--- Tema 6/Ejercicio10/JS/escuchador2.js
+++ Tema 6/Ejercicio10/JS/escuchador2.js
@@ -1,9 +1,10 @@
 let i=1;
 function colorear(){
     this.style.backgroundColor='lightblue';
-    if (i<=4) {
-        this.innerHTML+=`-${i++}`;
-    }
+    let contenido=this.innerHTML;
+    let letra= contenido.substr(-1,1);
+    this.innerHTML=`${letra}-${i++}`;
+    
 }
 
 Array.from(document.getElementById('idTabla').getElementsByTagName('td')).forEach(element=> {
