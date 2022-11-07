24506acdc922922749ec05ea78096d19389fd66b
manuhg13 <manuhg13@gmail.com>
2022-11-07 15:56:57 +0000

    f

--- Tema 6/Ejercicio11/JS/disparo.js
+++ Tema 6/Ejercicio11/JS/disparo.js
@@ -1,13 +1,25 @@
 let xDiana=50;
 let velocidad=10;
+let pasoDerecha;
+let pasoIzq;
 
-function moverDiana() {
-    xDiana+=velocidad;
-    document.getElementById('idDiana').style.left=`${xDiana}px`
-    if (document.getElementById('idDiana').style.left==document.getElementById('carrusel').style.width){
-        document.getElementById('idDiana').style.left=`-${xDiana}px`
-
-    }
+if (document.getElementById('idDiana').style.left==window.innerWidth){
+    pasoIzq=true;
+}else if(document.getElementById('idDiana').style.left==0){
+    pasoDerecha=true;
 }
 
+function moverDiana() {
+    if (pasoDerecha) {
+        xDiana+=velocidad;
+        document.getElementById('idDiana').style.left=`${xDiana}px`;
+        pasoIzq=false;
+    }else if(pasoIzq){
+        xDiana-=velocidad;
+        document.getElementById('idDiana').style.left=`${xDiana}px`;
+        pasoDerecha=false;
+    }
+    
+}
+console.log(document.getElementById('carrusel').style.width);
 let intervalDiana=setInterval(moverDiana,50);
