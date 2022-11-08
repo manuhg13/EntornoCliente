0a29939a4a35b1ae1c158c1b11f720e6071f1ace
manu <manuhg13@gmail.com>
2022-11-08 17:08:54 +0100

    d

--- Tema 6/Ejercicio11/JS/disparo.js
+++ Tema 6/Ejercicio11/JS/disparo.js
@@ -1,36 +1,3 @@
-/*let xDiana=0;
-let velocidad=10;
-
-let izquierda;
-let derecha;
-
-let intervalDiana=setInterval(limita,50);
-const diana=document.getElementById('idDiana');
-const carrusel=document.getElementById('carrusel');
-
-function moverDiana() {
-    
-    if (derecha=true){
-        xDiana+=velocidad;
-        diana.style.left=`${xDiana}px`;      
-    }else if(izquierda=true){
-        xDiana-=velocidad;
-        diana.style.left=`${xDiana}px`;
-    }
-    
-    
-}
-
-function limita() {
-    if (diana.style.left==(carrusel.clientWidth-30)) {
-        izquierda=true;
-        derecha=false;
-    }else if(diana.style.left==0){
-        derecha= true;
-        izquierda=false;
-    }
-    moverDiana();
-}*/
 
 let intervalDiana=setInterval(limita,50);
 const diana=document.getElementById('idDiana');
@@ -41,13 +8,6 @@ let timer;
 
 let izquierda;
 let derecha =true;
-
-/*timer=setInterval(function() {
-    diana.style.left=(left+=10) +"px";
-    if (left==window.screen.width-50) {
-        clearInterval(timer)
-    }
-}, 50);*/
 
 function limita() {
     if (left==window.screen.width-50) {
