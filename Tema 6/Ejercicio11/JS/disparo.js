1ccf77e712ea1830ce7316e1088f73e0c0aabb64
Manuel <manuhg13@gmail.com>
2022-11-10 09:40:50 +0000

    8

--- Tema 6/Ejercicio11/JS/disparo.js
+++ Tema 6/Ejercicio11/JS/disparo.js
@@ -7,6 +7,8 @@ const balon= document.getElementById('idBalon');
 let left=0;
 let timer;
 
+let posicionBalon= getComputedStyle(balon,null).getPropertyValue('left').slice(0,-2);
+
 let izquierda;
 let derecha =true;
 
@@ -21,9 +23,14 @@ function limita() {
     moverDiana();
 }
 
-balon.addEventListener('keydown',function(e) {
+document.addEventListener('keydown',function(e) {
+    console.log(e);
     if (e.key=="ArrowRight") {
-        balon.style.right+=10 + "px";
+        posicionBalon+=10;
+        balon.style.left+=`${posicionBalon}px `;
+    }else if (e.key == "ArrowLeft") {
+        posicionBalon-=10;
+        balon.style.left=`${posicionBalon}px`;
     }
 })
 function moverDiana() {
