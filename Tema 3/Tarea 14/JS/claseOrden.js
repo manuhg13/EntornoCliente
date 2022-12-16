94e027cdfba25d16d3f73a2f9c69bd9f21840180
Manuel <manuhg13@gmail.com>
2022-12-16 10:10:19 +0000

    v

---
+++ Tema 3/Tarea 14/JS/claseOrden.js
@@ -0,0 +1,36 @@
+export class Orden {
+    static MAX_PRODUCTOS=5;
+    static contadorOrdenes=0;
+
+    
+
+    constructor(){      
+        Orden.contadorOrdenes++;
+        this.idOrden=++Orden.contadorOrdenes;
+        this.productos=[];
+    }
+
+    agregarProducto(producto){
+        if(this.productos.length < Orden.MAX_PRODUCTOS){
+            this.productos.push(producto);
+            this.contadorProductosAgregados++;
+        }else{
+            console.log('No puede haber más de ' + Orden.MAX_PRODUCTOS + ' productos');
+        }
+    }
+
+    calcularTotal(){
+        let total=this.productos.reduce((importe, producto)=> importe+=producto.precio ,0);
+        return total;
+    }
+
+    mostrarOrden(){
+        return `------------------------
+        Orden: ${function name(params) {
+            
+        }}}${this.idOrden}${this.productos.reduce((texto,producto)=>texto+=`\n\t·${producto.toString()}`)}
+        ---------------------------
+        Total: ${this.calcularTotal().toLocaleString('es-ES',{style: 'currency',currency: 'EUR'})}`;
+    }
+
+}
