46ebfae046406ad2f2898e0fe3caaea5e1d80379
Manuel <manuhg13@gmail.com>
2023-01-09 10:34:58 +0000

    8

--- Tema 10/apuntes cookies/JS/cookies.js
+++ Tema 10/apuntes cookies/JS/cookies.js
@@ -1,3 +1,28 @@
+class Cookie{
+    constructor(clave,valor){
+        this.clave=clave;
+        this.valor=valor;
+    }
+}
+let arrayCookies=[];
+function getCookies(nombre) {
+    let todas=document.cookie;
+    let separadas=todas.split(";");
+    
+    separadas.forEach(cookie => {
+        let nueva=cookie.split('=');
+        arrayCookies.push(new Cookie(nueva[0],nueva[1]))
+    });
+
+    arrayCookies.forEach(cookie => {
+        if (cookie.valor==nombre) {
+            return alert("Clave:" + cookie.clave);
+        }
+    });
+
+    return alert("No existe esta cookie");
+
+}
 
 let i=1;
 function setCookie(nombre) {
@@ -7,3 +32,8 @@ function setCookie(nombre) {
 setCookie('Carlos');
 setCookie('Pedro');
 setCookie('Juan');
+
+let nombre=prompt('Busca la cookie:');
+getCookies(nombre);
+
+
