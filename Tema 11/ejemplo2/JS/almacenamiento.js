417bae5bfbc44d3cf6c279a727171f6e40bb8343
Manuel <manuhg13@gmail.com>
2023-01-12 09:51:36 +0000

    d

--- Tema 11/ejemplo2/JS/almacenamiento.js
+++ Tema 11/ejemplo2/JS/almacenamiento.js
@@ -1,6 +1,10 @@
 let campo=document.getElementById('idTexto');
 let boton=document.getElementById('boton');
 
+if (localStorage.texto) {
+    campo.value=localStorage.texto
+}
+
 
 function almacenar() {
     localStorage.setItem('Texto',campo.value);
