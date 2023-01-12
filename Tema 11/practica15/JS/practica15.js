445dbf0a3932b0154446a170c91025087d528f1a
manu <manuhg13@gmail.com>
2023-01-12 17:59:55 +0100

    d

---
+++ Tema 11/practica15/JS/practica15.js
@@ -0,0 +1,44 @@
+class Productos{
+
+    static contadorProductos=0;
+
+    constructor(nombre,precio){
+        this._nombre=nombre;
+        this._precio=precio;
+        this._idProducto=++Productos.contadorProductos; 
+    }
+
+    get nombre(){
+        return this._nombre.toUpperCase();
+    }
+
+    set nombre(nombre){
+        this._nombre=nombre.toUpperCase();
+    }
+
+    set precio(precio){
+        this._precio=precio.toLocaleString('es-ES',{style: 'currency',currency: 'EUR'});
+    }
+
+    get precio(){
+        return this._precio.toLocaleString('es-ES',{style: 'currency',currency: 'EUR'});
+    }
+
+    toString(){
+        return `${this.nombre} ${this.precio.toLocaleString('es-ES',{style: 'currency',currency: 'EUR'})}`;
+    }
+}
+
+
+document.getElementById('btnGuardar').addEventListener('click',guardar)
+
+function guardar() {
+    let nombre=document.getElementById('idNombre').value;
+    let precio=document.getElementById('idPrecio').value;
+
+    localStorage.setItem(nombre,precio);
+}
+
+function escribir() {
+    
+}
