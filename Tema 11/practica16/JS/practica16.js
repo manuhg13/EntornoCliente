696883f638832cd5d7a9253ea2ad3c5ccb658026
manu <manuhg13@gmail.com>
2023-01-15 20:02:23 +0100

    q

--- Tema 11/practica16/JS/practica16.js
+++ Tema 11/practica16/JS/practica16.js
@@ -6,7 +6,6 @@ let precioIn=document.getElementById('idPrecio');
 let formulario=document.getElementById('formulario');
 let arrayProductos=[];
 
-
 formulario.addEventListener('submit',function(evento) {
     evento.preventDefault();
     let valido=true;
@@ -23,23 +22,9 @@ formulario.addEventListener('submit',function(evento) {
     }
 })
 
-function guardar() {
-    cuerpo.innerHTML="";
-    if (arrayProductos.length==0 && localStorage.almacen){
-        let productosAlmacenados= localStorage.almacen;
-        let productosParseados= JSON.parse(productosAlmacenados);
-        productosParseados.forEach(element => {
-            let nuevoProducto= new Productos();
-            nuevoProducto.idProducto=element.idProducto;
-            nuevoProducto.nombre=element._nombre;
-            nuevoProducto.precio=element._precio;
-            arrayProductos.push(nuevoProducto);
-
-        });
-    }
-    arrayProductos.push(new Productos(nombreIn.value,precioIn.value))
-    
-    
+document.addEventListener('DOMContentLoaded',pintar);
+function pintar() {
+    tabla.innerHTML="";
     arrayProductos.forEach(producto => {
         let linea=document.createElement('tr');
 
@@ -59,12 +44,15 @@ function guardar() {
         let boton1=document.createElement('button');
         boton1.innerHTML='Eliminar';
         boton1.setAttribute('class','btn btn-danger m-1');
-        boton1.addEventListener('click',function() {
-            let padreBoton=this.parentNode;
-            let objetivo=padreBoton.parentNode;
-            cuerpo.removeChild(objetivo);
-        });
         funciones.appendChild(boton1);
+        boton1.addEventListener('click',function() {
+            let padreBoton=this.parentElement;
+            let objetivo=padreBoton.parentNode;
+            let valor=objetivo.children[0].innerHTML
+            tabla.removeChild(objetivo);
+            let indice= arrayProductos.findIndex( objeto=> objeto.idProducto===valor);
+            arrayProductos.slice(indice,indice-1);
+        });
 
         let boton2=document.createElement('button');
         boton2.innerHTML='Modificar';
@@ -75,9 +63,68 @@ function guardar() {
         funciones.appendChild(boton2);
         linea.appendChild(funciones);
 
-        cuerpo.appendChild(linea);
+        tabla.appendChild(linea);
     });
+}
+
+function guardar() {
+    if (arrayProductos.length==0 && localStorage.almacen){
+        let productosAlmacenados= localStorage.almacen;
+        let productosParseados= JSON.parse(productosAlmacenados);
+        productosParseados.forEach(element => {
+            let nuevoProducto= new Productos();
+            nuevoProducto.idProducto=element.idProducto;
+            nuevoProducto.nombre=element._nombre;
+            nuevoProducto.precio=element._precio;
+            arrayProductos.push(nuevoProducto);
+
+        });
+    }
+    arrayProductos.push(new Productos(nombreIn.value,precioIn.value))
     localStorage.almacen= JSON.stringify(arrayProductos);
+    
+    /*arrayProductos.forEach(producto => {
+        let linea=document.createElement('tr');
+
+        let id=document.createElement('td');
+        id.appendChild(document.createTextNode(producto.idProducto));
+        linea.appendChild(id);
+
+        let nombre=document.createElement('td');
+        nombre.appendChild(document.createTextNode(producto.nombre));
+        linea.appendChild(nombre);
+
+        let precio=document.createElement('td');
+        precio.appendChild(document.createTextNode(producto.precio));
+        linea.appendChild(precio);
+
+        let funciones=document.createElement('td');
+        let boton1=document.createElement('button');
+        boton1.innerHTML='Eliminar';
+        boton1.setAttribute('class','btn btn-danger m-1');
+        funciones.appendChild(boton1);
+        boton1.addEventListener('click',function() {
+            let padreBoton=this.parentElement;
+            let objetivo=padreBoton.parentNode;
+            let valor=objetivo.children[0].innerHTML
+            tabla.removeChild(objetivo);
+            let indice= arrayProductos.findIndex( objeto=> objeto.idProducto===valor);
+            arrayProductos.slice(indice,indice-1);
+        });
+
+        let boton2=document.createElement('button');
+        boton2.innerHTML='Modificar';
+        boton2.setAttribute('class','btn btn-info');
+        boton2.addEventListener('click',function() {
+            
+        });
+        funciones.appendChild(boton2);
+        linea.appendChild(funciones);
+
+        tabla.appendChild(linea);
+    });*/
+
+    pintar();
     
 }
 
