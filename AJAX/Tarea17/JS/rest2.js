86d5046f951d96d6ff5c001d4578a6b27aebf297
manuel <manuhg13@gmail.com>
2023-01-29 11:13:16 +0000

    d

---
+++ AJAX/Tarea17/JS/rest2.js
@@ -0,0 +1,174 @@
+const SERVER="http://192.168.2.205:3000/productos";
+//const http = new XMLHttpRequest();
+
+    /*document.getElementById('formulario').addEventListener('submit',(event)=>{
+        //event.preventDefault();
+        let idProd=document.getElementById('id').value
+        http.open('GET',`${SERVER}/${idProd}`);
+        http.send();
+        http.onreadystatechange=()=> {  
+    if (http.readyState===4) {
+        if (http.status===200) {
+            let objeto=JSON.parse(http.responseText);
+            document.getElementById('p1').innerHTML=Object.values(objeto);
+        }
+    }
+    };
+    });*/
+//-------------------------
+/*window.addEventListener('load',function () {  
+    document.getElementById('form').addEventListener('click',(event)=>{
+        let idProd=document.getElementById('id').value
+        if (isNaN(idProd) || idProd=='') {
+            alert('Debes introducir un número');
+        }else{
+            getProd(idProd)
+            .then((datos)=>{
+                document.getElementById('p1').innerHTML=Object.values(datos);
+            })
+
+            .catch((error)=> console.error(error))
+        }
+    });
+});
+function getProd(id) {
+    return new Promise((resolve,reject)=>{
+        
+        http.open('GET',`${SERVER}/${id}`,true);
+        http.send();
+        http.addEventListener('load',function(){
+            if (http.status===200) {
+                resolve(JSON.parse(http.responseText));
+            }else{
+                reject(`Error ${http.status} (${http.statusText}) ${id}`);
+            }
+        })
+        http.addEventListener('error',()=>{reject('Error en la petición HTTP')});
+    })
+}*/
+
+//--------------------------------------------------
+
+window.addEventListener('load',()=>{
+    $('#formulario').submit(function (evento) { 
+        evento.preventDefault();
+        let idProd=document.getElementById('id').value;
+
+        if (isNaN(idProd) || idProd.trim()=="") {
+            alert("Debes introducir un nº");
+        }else{
+            fetch(`${SERVER}/${idProd}`)
+            .then((response)=>{
+                if (!response.ok) {
+                    throw `Error ${response.status} de la BBDD: ${response.statusText}`;
+                }
+
+                return response.json();
+            })
+
+            .then((datos)=>{
+                document.getElementById('cuerpo').innerHTML="";
+                let linea =document.createElement('tr');
+
+                let id=document.createElement('td');
+                id.appendChild(document.createTextNode(datos.id));
+                linea.appendChild(id);
+
+                let nombre=document.createElement('td');
+                nombre.appendChild(document.createTextNode(datos.name));
+                linea.appendChild(nombre);
+
+                let descrip=document.createElement('td');
+                descrip.appendChild(document.createTextNode(datos.descrip));
+                linea.appendChild(descrip);
+
+                document.getElementById('cuerpo').appendChild(linea);
+                //document.getElementById('cuerpo').innerHTML=Object.values(datos);
+            })
+
+            .catch((error)=>console.error(error));
+        }
+        
+    });
+    
+});
+
+//-------------------------------------
+
+/*$.ajax({
+    type: "POST",
+    url: "http://192.168.2.205:3000/productos",
+    data: {id:"",name:"Impresora",descip:"Impresora gamming"},
+    dataType: "dataType",
+    success: function (datos) {
+        console.log("Esta");
+    }
+});*/
+
+
+//-----------------------------------------------------
+
+document.getElementById('introducir').addEventListener('submit',function(e) {
+    e.preventDefault();
+    const prod={
+        id: '',
+        name:document.getElementById('name').value,
+        descrip:document.getElementById('descrip').value
+    }
+
+    fetch(SERVER,{
+        method: 'POST',
+        body: JSON.stringify(prod),
+        headers:{
+            'Content-Type': 'application/json'
+        }
+    })
+    .then((response)=>{
+        if (!response.ok) {
+            throw `Error ${response.status} de la BBDD: ${response.statusText}`;
+        }
+
+        return response.json();
+    })
+    .then(datos=>{
+        alert("Datos bien");
+    })
+    .catch((error)=>console.error(error));
+});
+
+$('#listar').click(function (e) { 
+    e.preventDefault();
+    fetch(`${SERVER}`)
+            .then((response)=>{
+                if (!response.ok) {
+                    throw `Error ${response.status} de la BBDD: ${response.statusText}`;
+                }
+
+                return response.json();
+            })
+
+            .then((datos)=>{
+                document.getElementById('cuerpo').innerHTML="";
+
+                datos.forEach(element => {
+                    
+                    let linea =document.createElement('tr');
+    
+                    let id=document.createElement('td');
+                    id.appendChild(document.createTextNode(element['id']));
+                    linea.appendChild(id);
+    
+                    let nombre=document.createElement('td');
+                    nombre.appendChild(document.createTextNode(element['name']));
+                    linea.appendChild(nombre);
+    
+                    let descrip=document.createElement('td');
+                    descrip.appendChild(document.createTextNode(element['descrip']));
+                    linea.appendChild(descrip);
+                    document.getElementById('cuerpo').appendChild(linea);
+                });
+
+            })
+
+            .catch((error)=>console.error(error));
+});
