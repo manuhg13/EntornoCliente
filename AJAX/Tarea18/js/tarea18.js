4583f4f60fae44edb300169137aa6bb49efb6c01
manuel <manuhg13@gmail.com>
2023-02-03 15:30:57 +0000

    d

--- AJAX/Tarea18/js/tarea18.js
+++ AJAX/Tarea18/js/tarea18.js
@@ -1,4 +1,4 @@
-const SERVER="http://192.168.2.205:3000";
+const SERVER="http://192.168.31.205:4000";
 
 $('#listar').click(async function (e) { 
     e.preventDefault();
@@ -7,12 +7,56 @@ $('#listar').click(async function (e) {
         getTabla('/categorias'),
         getTabla('/productos')
     ])
+
+    catergorias.forEach((catergoria) => renderCategoria(catergoria));
+    productos.forEach((producto) => renderProducto(producto));
+
+    $('#borrar').removeAttr("disabled");
+    $(this).attr("disabled",true);
+
 });
 
 async function getTabla(tabla) {
     const response = await fetch(`${SERVER}${tabla}`);
 
     if (!response.ok){
-        
+        throw `Error ${response.status}de la BBDD: ${response.statusText}`
+    }
+
+    const datos = await response.json();
+
+    return datos;
+}
+
+function renderCategoria(cat) {
+    let primerNivel=document.createElement('li');
+    primerNivel.appendChild(document.createTextNode(cat.name));
+
+    let segundoNivel=document.createElement('ol');
+    segundoNivel.setAttribute('id',cat.id);
+    primerNivel.appendChild(segundoNivel);
+
+    document.getElementById('categorias').appendChild(primerNivel);
+}
+
+function renderProducto(prod) {
+    let elemento= document.createElement('li');
+    elemento.appendChild(document.createTextNode(prod.name));
+
+    if (prod.cat==1){
+        document.getElementById('1').appendChild(elemento);
+    }else if(prod.cat==2){
+        document.getElementById('2').appendChild(elemento);
     }
 }
+
+//---------------------------------------------------------------------------
+
+$('#borrar').click(function (e) { 
+    e.preventDefault();
+    
+    document.getElementById('categorias').innerHTML='';
+
+    $(this).attr("disabled",true);
+    $('#listar').removeAttr("disabled");
+});
