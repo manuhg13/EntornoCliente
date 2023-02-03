783ec1365465acbe6a874ddf4ae42c9da880fd6c
Manuel <manuhg13@gmail.com>
2023-02-03 09:07:05 +0000

    8

--- AJAX/Async/js/rest3.js
+++ AJAX/Async/js/rest3.js
@@ -54,3 +54,53 @@ async function getProducto(idProd) {
 
   return datos;
 }
+
+//-----------------------------------------------------------------------------------
+
+$("#listar").click(async function (e) {
+  e.preventDefault();
+  const datos = await todos();
+
+  document.getElementById("cuerpo").innerHTML = "";
+
+  datos.forEach((element) => {
+    let linea = document.createElement("tr");
+
+    let id = document.createElement("td");
+    id.appendChild(document.createTextNode(element["id"]));
+    linea.appendChild(id);
+
+    let nombre = document.createElement("td");
+    nombre.appendChild(document.createTextNode(element["name"]));
+    linea.appendChild(nombre);
+
+    let descrip = document.createElement("td");
+    descrip.appendChild(document.createTextNode(element["descrip"]));
+    linea.appendChild(descrip);
+    document.getElementById("cuerpo").appendChild(linea);
+
+    let funciones = document.createElement("td");
+    let boton1 = document.createElement("button");
+    boton1.innerHTML = "Modificar";
+    boton1.setAttribute("class", "btn btn-outline-dark m-1");
+    funciones.appendChild(boton1);
+    boton1.addEventListener("click", function () {
+      let padreBoton = this.parentNode;
+      let objetivo = padreBoton.parentNode;
+      document.getElementById("modId").value = objetivo.children[0].innerHTML;
+    });
+    linea.appendChild(funciones);
+  });
+});
+
+async function todos() {
+  const response = await fetch(`${SERVER}/productos`);
+
+  if (!response.ok) {
+    throw `Error ${response.status} de la BBDD: ${response.statusText}`;
+  }
+
+  const datos = await response.json();
+
+  return datos;
+}
